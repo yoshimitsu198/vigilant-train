5eaa1dff371b22896d901eaabcd6b21bd6027ad3
yoshimitsu198 <yoshimitsu198@users.noreply.github.com>
2025-11-30 05:18:05 +0200

    improve: add error messages

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# Updated iteration 5
+def function_5():
+    """Helper function for feature 5"""
+    return True
+
+def process_data_5(data):
+    """Process data for iteration 5"""
+    if data:
+        return data.upper()
+    return None
